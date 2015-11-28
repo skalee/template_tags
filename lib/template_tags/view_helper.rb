46cdc29b3eae8b34d7497f073ac02d73fd824cec
Sebastian Skałacki <skalee@gmail.com>
2015-11-28 19:28:11 +0100

    Render script tag with block-defined content

--- lib/template_tags/view_helper.rb
+++ lib/template_tags/view_helper.rb
@@ -1,7 +1,8 @@
 module TemplateTags
   module ViewHelper
 
-    def template_tag
+    def template_tag id, &block
+      content_tag :script, type: "text/x-tmpl", id: id, &block
     end
 
   end
--- spec/view_helper_spec.rb
+++ spec/view_helper_spec.rb
@@ -4,9 +4,31 @@ describe TemplateTags::ViewHelper, :type => :helper do
 
   describe "#template_tag" do
     let(:described_method){ helper.public_method :template_tag }
+    let(:id){ "some-id" }
+    let(:some_content){ "<div></div>".html_safe }
+    let(:rendered_script_element){ parsed_retval.css("script").first }
+    let(:parsed_retval){ Nokogiri::HTML::fragment retval }
+    let(:enclosed_html){ rendered_script_element.text }
 
-    it "is a helper method" do
-      expect(described_method).not_to be_nil
+    context "called with single parameter and a block" do
+      let(:retval){ described_method.call id do some_content end }
+
+      it "renders script tag as the only top-level element" do
+        expect(parsed_retval.children.count).to eq 1
+        expect(parsed_retval.children[0].name).to eq "script"
+      end
+
+      it "the rendered script tag has id equal to the only parameter passed" do
+        expect(rendered_script_element["id"]).to eq id
+      end
+
+      it "renders script tag of 'text/x-tmpl' type as a top-level element" do
+        expect(rendered_script_element["type"]).to eq "text/x-tmpl"
+      end
+
+      it "encloses passed block content" do
+        expect(enclosed_html).to eq some_content
+      end
     end
   end
 
