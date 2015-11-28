f02f17aec6988aded3fa0c7f7222449449e863b6
Sebastian Skałacki <skalee@gmail.com>
2015-11-28 17:48:04 +0100

    Configure RSpec-Rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "template_tags"
+
+require "rspec/rails"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
