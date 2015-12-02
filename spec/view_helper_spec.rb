0e9b4c8764d9282d053f48feb4bbbb47524fa7fe
Sebastian Skałacki <skalee@gmail.com>
2015-12-02 16:35:50 +0100

    Default behaviour specs extracted

--- spec/view_helper_spec.rb
+++ spec/view_helper_spec.rb
@@ -10,9 +10,7 @@ describe TemplateTags::ViewHelper, :type => :helper do
     let(:parsed_retval){ Nokogiri::HTML::fragment retval }
     let(:enclosed_html){ rendered_script_element.text }
 
-    context "called with single parameter and a block" do
-      let(:retval){ described_method.call id do some_content end }
-
+    shared_examples "default behaiour" do
       it "renders script tag as the only top-level element" do
         expect(parsed_retval.children.count).to eq 1
         expect(parsed_retval.children[0].name).to eq "script"
@@ -25,6 +23,12 @@ describe TemplateTags::ViewHelper, :type => :helper do
       it "renders script tag of 'text/x-tmpl' type as a top-level element" do
         expect(rendered_script_element["type"]).to eq "text/x-tmpl"
       end
+    end
+
+    context "called with single parameter and a block" do
+      let(:retval){ described_method.call id do some_content end }
+
+      include_examples "default behaiour"
 
       it "encloses passed block content" do
         expect(enclosed_html).to eq some_content
